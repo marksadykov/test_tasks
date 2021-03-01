d0587227717f9b826567e7ac1ea2839e6ea5c1db
marksadykov <sadykov.mark@gmail.com>
2021-03-01 13:15:18 +0300

    task1

--- Task1/task.js
+++ Task1/task.js
@@ -1,15 +1,16 @@
-function Class() {}
+function Class(
+) {this.kek = 'lll'; }
 Class.extend = function (desc) {
-    return function (el, options) {
-        desc.constructor.apply(this, [el, options]);
-        this.find = desc.find;
-        this.extend = function (extended) {
-            extended.constructor.bind(this);
-            this.handleEvent = extended.handleEvent;
-            this.toggle = extended.toggle;
-            return this;
-        }
-    };
+    // return function (el, options) {
+    //     desc.constructor.apply(this, [el, options]);
+    //     this.find = desc.find;
+    //     this.extend = function (extended) {
+    //         extended.constructor.bind(this);
+    //         this.handleEvent = extended.handleEvent;
+    //         this.toggle = extended.toggle;
+    //         return this;
+    //     }
+    // };
 
     // return {
     //     extend: (extended) =>  function (menu){
@@ -21,6 +22,24 @@ Class.extend = function (desc) {
     //         this.toggle = extended.toggle;
     //     }
     // };
+
+    // this.prototype = desc;
+    console.log(desc);
+    this.prototype = {
+        el: desc.el,
+        options: desc.options,
+        constructor: desc.constructor,
+        find: desc.find,
+        handleEvent: desc.handleEvent,
+        toggle: desc.toggle,
+        // extend: (extended) => {
+        //     console.log(extended);
+        //     this.prototype = extended;
+        //     this.prototype.haha = 'hahah';
+        // }
+    };
+    console.log(this);
+    return this;
 };
 
 /** @class Widget */
@@ -35,35 +54,46 @@ var Widget = Class.extend(/** @lends Widget.prototype */{
     },
 });
 
-const menu = document.getElementById('menu');
-const dd = new Widget(menu);
-console.log(dd);
+// const menu = document.getElementById('menu');
+// const ddd = new Widget();
+// for(let prop in ddd) {
+//     console.log(prop);
+// }
+//
+// ddd.constructor(menu, 'l');
+
 
 /** @class Dropdown */
 /** @extends Widget */
-// var Dropdown = Widget.extend(/** @lends Dropdown.prototype */{
-//     constructor: function () {
-//         Widget.apply(this, arguments);
-//         this.find('.js-ctrl').addEventListener('click', this.handleEvent);
-//     },
-//
-//     handleEvent: function (evt) {
-//         this.toggle();
-//     },
-//
-//     toggle: function () {
-//         var menu = this.find('.js-menu');
-//         menu.classList.toggle('collapsed');
-//     }
-// });
+var Dropdown = Widget.extend(/** @lends Dropdown.prototype */{
+    constructor: function () {
+        Widget.apply(this, arguments);
+        this.find('.js-ctrl').addEventListener('click', this.handleEvent);
+    },
+
+    handleEvent: function (evt) {
+        this.toggle();
+    },
+
+    toggle: function () {
+        var menu = this.find('.js-menu');
+        menu.classList.toggle('collapsed');
+    }
+});
 
 //Используем
-// const menu = document.getElementById('menu');
-// const dd = new Dropdown(menu);
+const menu = document.getElementById('menu');
+const dd = new Dropdown();
+for(let prop in dd) {
+    console.log(prop);
+}
+
+dd.constructor(menu, 'j');
+console.log(dd);
 
 console.log('dd is Class:', dd instanceof Class);
 console.log('dd is Widget:', dd instanceof Widget);
-// console.log('dd is Dropdown:', dd instanceof Dropdown);
+console.log('dd is Dropdown:', dd instanceof Dropdown);
 
 export default dd;
 
